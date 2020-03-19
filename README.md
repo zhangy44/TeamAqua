5938a6e2ef22bec77f8b887025b23c75af097719
zhangy44 <yang3377zhang@gmail.com>
2020-03-19 17:23:46 -0400

    Initial Commit, enable lovely little car images and key control

---
+++ README.md
@@ -0,0 +1,12 @@
+# Team Aqua 2020
+
+Big-Co Studio Project
+
+
+## Installing
+
+    git clone https://github.com/volkhin/RoadTrafficSimulator
+    cd RoadTrafficSimulator
+    npm install
+
+And open index.html in your browser. Use **gulp** to rebuild project.
---
+++ js/control.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    // $("#carNumberControl").on("click", function (e) {
+    //     let carNumber = Math.floor(Math.random() * 200);
+    //     world.carsNumber = carNumber;
+    //     console.log(`Car Number ${carNumber}`)
+    //     $(this).html(`Car Number ${carNumber}`)
+    // })
+    $(document).keydown(function(event) {
+
+        var key = (event.keyCode ? event.keyCode : event.which);
+        switch(key){
+            case 38:
+                // alert("up")
+                adjustCarNumber(true)
+                break
+            case 40:
+                adjustCarNumber(false)
+                // alert("up")
+                break
+        }
+
+    })
+    function adjustCarNumber(isUp){
+        let increment = 10;
+        let max = 200;
+        let min = 10;
+        let currentCarsNumber = world.carsNumber;
+        if(isUp){
+            if((currentCarsNumber + increment) > max){
+                world.carsNumber = max
+            }   else    {
+                world.carsNumber += increment
+            }
+        }   else    {
+            if((currentCarsNumber - increment) < min){
+                world.carsNumber = min;
+            }   else    {
+                world.carsNumber -= increment
+            }
+        }
+    }
+})
